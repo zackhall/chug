fee0ed123ca3600f078ec28d371fcb5a421e5f7a
Zack Hall <zackhall@outlook.com>
2016-02-29 08:28:56 -0800

    Add basic styling, ability to parse posts

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var browserSync = require('browser-sync').create(),
   gulpSwig = require('gulp-swig'),
   marked = require('gulp-marked'),
   path = require('path'),
+  rename = require('gulp-rename'),
   sass = require('gulp-sass'),
   swig = require('swig'),
   through = require('through2');
@@ -12,16 +13,20 @@ var browserSync = require('browser-sync').create(),
 var bourbon = require('node-bourbon').includePaths,
   neat = require('node-neat').includePaths;
 
-var config = function() {
-  return require('./config.json');
-};
+var site = require('./config.json');
 
-gulp.task('index', function() {
+var swigDefaults = {
+  defaults: { cache: false }
+}
+
+var postNamePattern = /(\d{4})-(\d{1,2})-(\d{1,2})-(.*)/;
+
+gulp.task('index', ['posts'], function() {
   gulp.src('src/index.html')
-    .pipe(data(config))
-    .pipe(gulpSwig({ 
-      defaults: { cache: false }
+    .pipe(data(function() {
+      return site;
     }))
+    .pipe(gulpSwig(swigDefaults))
     .pipe(gulp.dest('dist'));
 });
 
@@ -30,6 +35,21 @@ gulp.task('posts', function() {
     .pipe(frontMatter({ property: 'page', remove: true }))
     .pipe(marked())
     .pipe(applyTemplate('src/partials/_post.html'))
+    .pipe(parsePostName())
+    .pipe(collectPosts())
+    .pipe(rename(function (path) {
+      path.extname = ".html";
+      var match = postNamePattern.exec(path.basename);
+      if (match)
+      {
+        var year = match[1];            
+        var month = match[2];
+        var day = match[3];
+    
+        path.dirname = year + '/' + month + '/' + day;
+        path.basename = match[4];
+      }            
+    }))
     .pipe(gulp.dest('dist/posts'));
 });
 
@@ -48,7 +68,7 @@ gulp.task('serve', ['build'], function() {
     server: "./dist"
   });
 
-  gulp.watch('src/index.html', ['index', 'reload']);
+  gulp.watch('src/**/*.html', ['build', 'reload']);
   gulp.watch('src/posts/**/*.md', ['posts', 'reload']);
   gulp.watch('src/scss/**/*.scss', ['sass', 'reload']);
 });
@@ -57,9 +77,17 @@ gulp.task('reload', function() {
   browserSync.reload();
 })
 
-gulp.task('build', ['index', 'posts', 'sass']);
+gulp.task('build', ['posts', 'index', 'sass'], function() {
+  gulp.src('assets/*')
+    .pipe(gulp.dest('dist/assets/'));
+});
 
-gulp.task('default', ['build']);
+gulp.task('clean', function() {
+  return gulp.src('dist', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('default', ['serve']);
 
 function applyTemplate(templateFile) {
   var tpl = swig.compileFile(path.join(__dirname, templateFile));
@@ -73,4 +101,38 @@ function applyTemplate(templateFile) {
     this.push(file);
     cb();
   });
+}
+
+function collectPosts() {
+  posts = [];
+  return through.obj(function (file, enc, cb) {
+    posts.push(file.page);    this.push(file);
+    cb();
+  }, function (cb) {
+    posts.sort(function (a, b) {
+      return b.date - a.date;
+    });
+    site.posts = posts;
+    list2 = posts;
+    cb();
+  });
+}
+
+function parsePostName() {
+  return through.obj(function (file, enc, cb) {
+    var basename = path.basename(file.path, '.md');
+    var match = postNamePattern.exec(basename);
+    if (match)
+    {
+      var year     = match[1];            
+      var month    = match[2];
+      var day      = match[3];
+      var basename = match[4];
+      file.page.date = new Date(year, month, day);
+      file.page.url  = '/posts/' + year + '/' + month + '/' + day + '/' + basename; // + '.html';
+    }
+    
+    this.push(file);
+    cb();
+  })
 }
