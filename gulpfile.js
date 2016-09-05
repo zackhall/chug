9c964eba0bf3f4d40a5e039ff24740fe536fdd44
Ramin Vakilian <ramin@hybridmarketing.com.au>
2016-09-05 16:34:35 +0930

    Loads gulp plugins automatically and adds newly introduced tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,15 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+plugins = require('gulp-load-plugins')();
 
-var plugins = {
-  browserSync: require('browser-sync').create(),
-  concat: require('gulp-concat'),
-  data: require('gulp-data'),
-  del: require('del'),
-  frontMatter: require('gulp-front-matter'),
+plugins = Object.assign(plugins, {
+  //Custom requires
   md: require('./gulp/lib/md'),
-  minify: require('gulp-minify-css'),
   nunjucks: require('./gulp/lib/nunjucks'),
-  rename: require('gulp-rename'),
-  sass: require('gulp-sass'),
-  surge: require('gulp-surge')
-};
+  browserSync: require('browser-sync').create(),
+  del: require('del'),
+  mainBowerFiles: require('main-bower-files'),
+  runSequence: require('run-sequence')
+});
 
 var site = require('./config.json');
 
@@ -20,16 +17,38 @@ function getTask(task) {
   return require('./gulp/tasks/' + task)(gulp, plugins, site);
 }
 
-gulp.task('assets', getTask('assets'));
-gulp.task('build', ['posts', 'pages', 'root', 'sass', 'assets']);
 gulp.task('clean', getTask('clean'));
-gulp.task('default', ['serve']);
-gulp.task('deploy', ['build'], getTask('deploy'));
-gulp.task('posts', getTask('posts'));
-gulp.task('pages', ['pages:md', 'pages:html']);
+
 gulp.task('pages:html', getTask('pages/pages_html'));
 gulp.task('pages:md', getTask('pages/pages_md'));
-gulp.task('reload', function() { browserSync.reload(); });
+gulp.task('posts', getTask('posts'));
 gulp.task('root', ['posts'], getTask('root'));
+gulp.task('pages', ['pages:md', 'pages:html']);
+
+gulp.task('bower', getTask('bower'));
+gulp.task('vendorjs', getTask('vendor-js'));
+gulp.task('vendorcss', getTask('vendor-css'));
+gulp.task('vendor', function(cb){
+  plugins.runSequence('bower', 'vendorjs', 'vendorcss', 'reload');
+  cb();
+});
+
+gulp.task('custom-fonts', getTask('custom-fonts'));
+gulp.task('google-fonts', getTask('google-fonts'));
+gulp.task('fonts', ['custom-fonts', 'google-fonts']);
+
+gulp.task('scripts', getTask('scripts'));
 gulp.task('sass', getTask('sass'));
+gulp.task('images', getTask('images'));
+
+gulp.task('build', function(cb){
+  plugins.runSequence('vendor', 'posts', 'root', 'pages', ['scripts', 'images', 'sass', 'fonts'], 'reload');
+  cb();
+});
+
 gulp.task('serve', ['build'], getTask('serve'));
+gulp.task('reload', function() { setTimeout(function(){
+  plugins.browserSync.reload();
+}, 1000); });
+gulp.task('default', ['serve']);
+gulp.task('deploy', ['build'], getTask('deploy'));
